5facc5c042c1ad0be5e79814d1311357ddde8cab
Greg Vaggalis <gvaggalis@gmail.com>
2020-05-06 15:40:04 -0500

    added dist

---
+++ src/components/Task.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':aTask.completed}">
+        <p>
+            <input type="checkbox" v-on:click="markComplete">
+            {{aTask.title}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"task",
+    props:['aTask'],
+    methods:
+    {
+        markComplete(){
+            this.aTask.completed = !this.aTask.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item{
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete{
+        text-decoration: line-through;
+    }
+</style>
